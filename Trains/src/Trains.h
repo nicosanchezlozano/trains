1f3796433126aa0c3407ba214ab67bb0cace2545
Nicolas <nicopelos@hotmail.com>
2018-03-13 11:14:12 +0100

    download Trains

---
+++ Trains/src/Trains.h
@@ -0,0 +1,17 @@
+/*
+ * Trains.h
+ *
+ *  Created on: 13/03/2018
+ *      Author: usuario
+ */
+
+#ifndef TRAINS_H_
+#define TRAINS_H_
+
+class Trains {
+public:
+	Trains();
+	virtual ~Trains();
+};
+
+#endif /* TRAINS_H_ */
